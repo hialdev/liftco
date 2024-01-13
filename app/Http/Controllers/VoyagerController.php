a3153578a3d4da7fb3230db2ebe4a6e8a84c8d5e
hialdev <al.developer12@gmail.com>
2024-01-13 11:33:42 +0700

    none custom view

--- app/Http/Controllers/VoyagerController.php
+++ app/Http/Controllers/VoyagerController.php
@@ -19,120 +19,120 @@ class VoyagerController extends \TCG\Voyager\Http\Controllers\VoyagerBaseControl
         return parent::getContentBasedOnType($request, $slug, $row, $options);
     }
 
-    //***************************************
-    //                ______
-    //               |  ____|
-    //               | |__
-    //               |  __|
-    //               | |____
-    //               |______|
-    //
-    //  Edit an item of our Data Type BR(E)AD
-    //
-    //****************************************
+    // //***************************************
+    // //                ______
+    // //               |  ____|
+    // //               | |__
+    // //               |  __|
+    // //               | |____
+    // //               |______|
+    // //
+    // //  Edit an item of our Data Type BR(E)AD
+    // //
+    // //****************************************
 
-    public function edit(Request $request, $id)
-    {
-        $slug = $this->getSlug($request);
+    // public function edit(Request $request, $id)
+    // {
+    //     $slug = $this->getSlug($request);
 
-        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+    //     $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
-        if (strlen($dataType->model_name) != 0) {
-            $model = app($dataType->model_name);
-            $query = $model->query();
+    //     if (strlen($dataType->model_name) != 0) {
+    //         $model = app($dataType->model_name);
+    //         $query = $model->query();
 
-            // Use withTrashed() if model uses SoftDeletes and if toggle is selected
-            if ($model && in_array(SoftDeletes::class, class_uses_recursive($model))) {
-                $query = $query->withTrashed();
-            }
-            if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
-                $query = $query->{$dataType->scope}();
-            }
-            $dataTypeContent = call_user_func([$query, 'findOrFail'], $id);
-        } else {
-            // If Model doest exist, get data from table name
-            $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
-        }
+    //         // Use withTrashed() if model uses SoftDeletes and if toggle is selected
+    //         if ($model && in_array(SoftDeletes::class, class_uses_recursive($model))) {
+    //             $query = $query->withTrashed();
+    //         }
+    //         if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
+    //             $query = $query->{$dataType->scope}();
+    //         }
+    //         $dataTypeContent = call_user_func([$query, 'findOrFail'], $id);
+    //     } else {
+    //         // If Model doest exist, get data from table name
+    //         $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
+    //     }
 
-        foreach ($dataType->editRows as $key => $row) {
-            $dataType->editRows[$key]['col_width'] = isset($row->details->width) ? $row->details->width : 100;
-        }
+    //     foreach ($dataType->editRows as $key => $row) {
+    //         $dataType->editRows[$key]['col_width'] = isset($row->details->width) ? $row->details->width : 100;
+    //     }
 
-        // If a column has a relationship associated with it, we do not want to show that field
-        $this->removeRelationshipField($dataType, 'edit');
+    //     // If a column has a relationship associated with it, we do not want to show that field
+    //     $this->removeRelationshipField($dataType, 'edit');
 
-        // Check permission
-        $this->authorize('edit', $dataTypeContent);
+    //     // Check permission
+    //     $this->authorize('edit', $dataTypeContent);
 
-        // Check if BREAD is Translatable
-        $isModelTranslatable = is_bread_translatable($dataTypeContent);
+    //     // Check if BREAD is Translatable
+    //     $isModelTranslatable = is_bread_translatable($dataTypeContent);
 
-        // Eagerload Relations
-        $this->eagerLoadRelations($dataTypeContent, $dataType, 'edit', $isModelTranslatable);
+    //     // Eagerload Relations
+    //     $this->eagerLoadRelations($dataTypeContent, $dataType, 'edit', $isModelTranslatable);
 
-        $view = $this->getPageView ($slug);
+    //     $view = $this->getPageView ($slug);
 
-        return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable'));
-    }
+    //     return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable'));
+    // }
 
-    //***************************************
-    //
-    //                   /\
-    //                  /  \
-    //                 / /\ \
-    //                / ____ \
-    //               /_/    \_\
-    //
-    //
-    // Add a new item of our Data Type BRE(A)D
-    //
-    //****************************************
+    // //***************************************
+    // //
+    // //                   /\
+    // //                  /  \
+    // //                 / /\ \
+    // //                / ____ \
+    // //               /_/    \_\
+    // //
+    // //
+    // // Add a new item of our Data Type BRE(A)D
+    // //
+    // //****************************************
 
-    public function create(Request $request)
-    {
-        $slug = $this->getSlug($request);
+    // // public function create(Request $request)
+    // // {
+    // //     $slug = $this->getSlug($request);
 
-        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+    // //     $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
-        // Check permission
-        $this->authorize('add', app($dataType->model_name));
+    // //     // Check permission
+    // //     $this->authorize('add', app($dataType->model_name));
 
-        $dataTypeContent = (strlen($dataType->model_name) != 0)
-                            ? new $dataType->model_name()
-                            : false;
+    // //     $dataTypeContent = (strlen($dataType->model_name) != 0)
+    // //                         ? new $dataType->model_name()
+    // //                         : false;
 
-        foreach ($dataType->addRows as $key => $row) {
-            $dataType->addRows[$key]['col_width'] = $row->details->width ?? 100;
-        }
+    // //     foreach ($dataType->addRows as $key => $row) {
+    // //         $dataType->addRows[$key]['col_width'] = $row->details->width ?? 100;
+    // //     }
 
-        // If a column has a relationship associated with it, we do not want to show that field
-        $this->removeRelationshipField($dataType, 'add');
+    // //     // If a column has a relationship associated with it, we do not want to show that field
+    // //     $this->removeRelationshipField($dataType, 'add');
 
-        // Check if BREAD is Translatable
-        $isModelTranslatable = is_bread_translatable($dataTypeContent);
+    // //     // Check if BREAD is Translatable
+    // //     $isModelTranslatable = is_bread_translatable($dataTypeContent);
 
-        // Eagerload Relations
-        $this->eagerLoadRelations($dataTypeContent, $dataType, 'add', $isModelTranslatable);
+    // //     // Eagerload Relations
+    // //     $this->eagerLoadRelations($dataTypeContent, $dataType, 'add', $isModelTranslatable);
 
-        $view = $this->getPageView ($slug);
+    // //     $view = $this->getPageView ($slug);
 
-        return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable'));
-    }
+    // //     return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable'));
+    // // }
 
-    private function getPageView ($slug)
-    {
-        $template = 'voyager::bread.edit-add';
+    // // private function getPageView ($slug)
+    // // {
+    // //     $template = 'voyager::bread.edit-add';
 
-        /* if (view()->exists("admin.$slug-form")) {
-            $template = "admin.$slug-form";
-        } */
+    // //     /* if (view()->exists("admin.$slug-form")) {
+    // //         $template = "admin.$slug-form";
+    // //     } */
 
-        if (in_array($slug, ['services', 'spareparts'])){
-            $template = "admin.contents-form";
-        }else if (in_array($slug, ['posts', 'products', 'pages'])){
-            $template = "admin.$slug-form";
-        }
+    // //     if (in_array($slug, ['services', 'spareparts'])){
+    // //         $template = "admin.contents-form";
+    // //     }else if (in_array($slug, ['posts', 'products', 'pages'])){
+    // //         $template = "admin.$slug-form";
+    // //     }
 
-        return $template;
-    }
+    // //     return $template;
+    // // }
 }
